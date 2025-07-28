9d5122dac47abf2fa9411ddfa5ddad0c0451f4be
Pascal Ordano <pascalordanodev@gmail.com>
2025-07-28 00:59:54 -0300

    updates but styles not working

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -6,6 +6,8 @@ import { supabase } from "@/lib/supabase";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/hooks/useUser";
+import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
 
 type Entry = {
   id: string,
@@ -18,8 +20,9 @@ type Entry = {
 export default function Journal() {
 
   const [entries, setEntries] = useState<Entry[]>([]);
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
   const [value, setValue] = useState('');
-  // const [loading, setLoading] = useState(false);
+//   const [loading, setLoading] = useState(false);
 
   const JOURNAL_ENTRIES_TABLE = 'journal_entries';
   const { user, loading } = useUser();
@@ -30,15 +33,12 @@ export default function Journal() {
     if(!loading && user === null){
       router.push('/login');
     }
+    fetchEntries();
   }, [user, loading, router]);
 
   if(loading || !user) return null;
 
 
-  
-  // useEffect(() => {
-  //     fetchEntries();
-  // }, []);
   
 
 
@@ -75,19 +75,39 @@ export default function Journal() {
     fetchEntries();
   }
 
+
+  const handleSignOut = async() => {
+    await supabase.auth.signOut();
+    router.push('/');
+  }
+
   return (
     <main className="min-h-screen bg-gray-100 px-4 py-8 md:px-12">
       <div className="max-w-2xl mx-auto space-y-6">
         <header className="text-center">
           <h1 className="text-3xl font-bold text-gray-800">My Journal</h1>
           <p className="text-gray-500 text-sm mt-1">Today is {/* you can render date here */}</p>
+          <button
+                onClick={handleSignOut}
+                className="mt-2 text-sm text-blue-600 underline hover:text-blue-800"
+                >
+                Sing Out
+          </button>
         </header>
-
         <section className="bg-white shadow-md rounded-xl p-6 space-y-4">
+            <div className="bg-white rounded-xl shadow p-4">
+                <h2 className="text-lg font-semibold mb-4">Select a Day</h2>
+                <Calendar
+                mode="single"
+                selected={selectedDate}
+                onSelect={setSelectedDate}
+                className="rounded-md border"
+                />
+            </div>
           <h2 className="text-xl font-semibold text-gray-700">New Entry</h2>
           <form className="space-y-4" onSubmit={(e) => handleSubmit(e)}>
             <textarea
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
               rows={5}
               placeholder="Write something about your day..."
               value={value}
