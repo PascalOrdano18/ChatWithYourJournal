ea82588985b06a57cc21627571a600120af2f4a2
Pascal Ordano <pascalordanodev@gmail.com>
2025-08-02 18:40:48 -0300

    ai integrada con vercel ai sdk

--- app/api/ask/route.ts
+++ app/api/ask/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
-
 import { supabase } from "@/lib/supabase";
+import { generateText } from "ai"
+import { google } from "@ai-sdk/google"
 
 export async function POST(req: Request){
     const { question } = await req.json();
+    
+    if (!process.env.GOOGLE_GENERATIVE_AI_API_KEY) {
+        return NextResponse.json({ answer: "Missing Gemini API key." }, { status: 500 });
+    }
 
     const { data: entries, error } = await supabase
     .from('journal_entries')
@@ -11,8 +16,12 @@ export async function POST(req: Request){
     .order('entry_date', { ascending: false })
     .limit(30);
 
+    if(error || !entries){
+        return NextResponse.json({ answer: "Hubo un problema sorry" });
+    }
+
     const context = entries
-    .map((entry) => {
+    .map(entry => {
         `- ${entry.entry_date}: ${entry.content}`
     }).join('\n');
 
@@ -26,7 +35,24 @@ export async function POST(req: Request){
     `;
 
 
+    // SI USASE OPENAI
+    // const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    // const completion = await openai.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    // messages: [
+    //   { role: "system", content: "You are a helpful journal assistant." },
+    //   { role: "user", content: prompt },
+    // ],
+    // })
+    // const answer = completion.choices[0].message.content;
+
+
+   const { text } = await generateText({
+    model: google("models/gemini-2.0-flash-exp"),
+    prompt: prompt
+   })
+
     return NextResponse.json({
-        answer: `la pregunta fue ${question}`
+        answer: text
     });
 }
